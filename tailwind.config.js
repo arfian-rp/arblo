f34242dae868d77fe2ef8aacbaafd4a2f4ed1db0
arfian-rp <arfianpradana07@gmail.com>
2022-05-17 10:50:01 +0700

    ok

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,17 @@ module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      // colors: {
+      //   primary: "#475569",
+      //   secondary: "#e2e8f0",
+      //   secHov: "#f8fafc",
+      //   priHov: "#475569",
+      // },
       colors: {
-        primary: "#475569",
-        secondary: "#e2e8f0",
-        secHov: "#f8fafc",
-        priHov: "#475569",
+        secondary: "#475569",
+        primary: "#e2e8f0",
+        priHov: "#f8fafc",
+        secHov: "#475569",
       },
     },
   },
