4fd0bd7a80e999d787a5ffacc75c259f44335aef
arfian-rp <arfianpradana07@gmail.com>
2022-05-17 22:49:12 +0700

    finish

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -15,7 +15,7 @@ export default function Layout({ title, children, username, isAuth, description 
       <Head>
         <title>{title}</title>
         <meta charSet="utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content={description} />
         <meta name="keywords" content="arblo, article, blog, article blog, blog article, arfian blogm arfian article" />
