dd7a829098239d2d7d282dda41ffc354524b1580
arfian-rp <arfianpradana07@gmail.com>
2022-05-17 23:20:50 +0700

    ok

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -14,6 +14,7 @@ export default function Layout({ title, children, username, isAuth, description 
     <>
       <Head>
         <title>{title}</title>
+        <meta name="google-site-verification" content="dxLF11ksIfjF9iH6LmjWJjZ8KR_fktxj8Ihqj-tTdd4" />
         <meta charSet="utf-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
