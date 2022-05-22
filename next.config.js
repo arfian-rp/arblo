f0636b719b0bbe1156cc13eb8745586644d1d703
arfian-rp <arfianpradana07@gmail.com>
2022-05-22 22:49:44 +0700

    ok

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    concurrentFeatures: true,
-    serverComponents: true,
-    reactRoot: true,
-  },
 };
 
 module.exports = nextConfig;
