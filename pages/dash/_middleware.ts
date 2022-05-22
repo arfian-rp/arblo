ac8ae21d5897e2fa92b31fbe973212578009813e
arfian-rp <arfianpradana07@gmail.com>
2022-05-22 22:37:52 +0700

    ok

--- pages/dash/_middleware.ts
+++ pages/dash/_middleware.ts
@@ -2,7 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 import connectDb from "../../utils/connectDb";
 import verifyToken from "../../utils/verifyToken";
 
-export default async function middleware(req: NextRequest) {
+export async function middleware(req: NextRequest) {
   connectDb();
   const { userToken } = await verifyToken(req.cookies.refreshToken);
   if (userToken) {
