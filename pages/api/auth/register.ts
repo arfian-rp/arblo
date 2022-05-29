164eaf4b1bc7e3b412116426fea739fdb6330d73
arfian-rp <arfianpradana07@gmail.com>
2022-05-29 21:51:22 +0700

    ok

--- pages/api/auth/register.ts
+++ pages/api/auth/register.ts
@@ -3,25 +3,19 @@ import { NextApiRequest, NextApiResponse } from "next";
 import UserModel from "../../../model/UserModel";
 import connectDb from "../../../utils/connectDb";
 import { resUtilError, resUtilSuccess } from "../../../utils/resUtil";
-import verifyToken from "../../../utils/verifyToken";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "POST") {
-    const { userToken } = await verifyToken(req.cookies.refreshToken);
-    if (!userToken) {
-      connectDb();
-      const { username, email, password, confirm } = req.body;
-      if (password.length >= 8) {
-        if (password === confirm) {
-          const hash = await bcrypt.hash(password, 10);
-          new UserModel({ username: username.toLowerCase(), email: email.toLowerCase(), password: hash })
-            .save()
-            .then(() => {
-              resUtilSuccess(res);
-            })
-            .catch(() => resUtilError(res));
-        } else resUtilError(res);
-      } else resUtilError(res);
+    connectDb();
+    const { username, email, password, confirm } = req.body;
+    if (password.length >= 8 && password === confirm) {
+      const hash = await bcrypt.hash(password, 10);
+      new UserModel({ username: username.toLowerCase(), email: email.toLowerCase(), password: hash })
+        .save()
+        .then(() => {
+          resUtilSuccess(res);
+        })
+        .catch(() => resUtilError(res));
     } else resUtilError(res);
   } else resUtilError(res);
 }
