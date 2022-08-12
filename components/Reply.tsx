0cfdd360b3cf4f527a972da47270d7e905e3f657
arfian pradana <arfianpradana07@gmail.com>
2022-08-12 21:46:37 +0700

    fitur photo

--- components/Reply.tsx
+++ components/Reply.tsx
@@ -26,7 +26,7 @@ export default function Reply({ reply, _idPost, mode = false }: Props) {
 
   return (
     <div className="text-lg mx-auto rounded-lg my-1 w-[384px] md:w-[600px] border-2 border-primary hover:border-black">
-      <div className="border-2 border-b-primary text-left p-3 flex justify-between text-2xl cursor-pointer">
+      <div className="text-left p-3 flex justify-between text-2xl cursor-pointer">
         <div onClick={() => router.push(`/u/${reply.author}`)} className="flex items-center gap-3 hover:text-secondary">
           {reply.author}
         </div>
