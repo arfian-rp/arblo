cc6276f2bd3d5bfb6f0293e339ef985cd978cb59
arfian pradana <arfianpradana07@gmail.com>
2022-08-12 19:56:08 +0700

    ok

--- components/PostComp.tsx
+++ components/PostComp.tsx
@@ -46,7 +46,7 @@ export default function PostComp({ _id, title, body, author, postedAt, mode = fa
         </div>
         {image ? (
           <div className="mx-auto">
-            <Image className="border-2 border-black w-[384px] md:w-[600px]" src={`https://res.cloudinary.com/arfian07/image/upload/c_fill,w_600/${image}`} width={600} alt={title} />
+            <Image className="border-2 border-black w-[384px] md:w-[600px]" src={`https://res.cloudinary.com/arblo/image/upload/c_fill,w_600/${image}`} width={600} alt={title} />
           </div>
         ) : (
           <></>
