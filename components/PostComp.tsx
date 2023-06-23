aa62fedec82026cccdd469f110364ebce657349c
arfian pradana <arfianpradana07@gmail.com>
2023-06-23 10:36:14 +0700

    e4r224

--- components/PostComp.tsx
+++ components/PostComp.tsx
@@ -37,7 +37,7 @@ export default function PostComp({ _id, title, body, author, postedAt, mode = fa
       data: { _id, image },
       result: () => (window.location.pathname = `/u/${author}`),
     };
-    if (confirm(`delete post: '${title}'?`)) {
+    if (confirm(`delete '${title?`post: ${title}`:"this post"}'?`)) {
       req(param);
     }
   }
