70d2d4c4c6696c1b766a1bde154ae481215c8efe
arfian pradana <arfianpradana07@gmail.com>
2024-04-06 13:07:07 +0700

    efqw3w

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,12 @@ module.exports = {
   content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      colors: {
-        primary: "#475569",
-        secondary: "#e2e8f0",
-        secHov: "#f8fafc",
-        priHov: "#475569",
-      },
+      // colors: {
+      //   primary: "#475569",
+      //   secondary: "#e2e8f0",
+      //   secHov: "#f8fafc",
+      //   priHov: "#475569",
+      // },
       colors: {
         secondary: "#475569",
         primary: "#e2e8f0",
